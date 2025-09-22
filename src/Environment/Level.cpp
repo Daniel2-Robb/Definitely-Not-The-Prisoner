a40cd0cccae5fbdd047f5aada70f37a1e0ceb7f9
SpiderBinderAgain <elliott2.ritchie@live.uwe.ac.uk>
2025-09-22 16:25:21 +0100

    add basic Level class

---
+++ src/Environment/Level.cpp
@@ -0,0 +1,32 @@
+
+#include "Level.h"
+
+Level::Level(sf::Texture tileset) : tileset(tileset)
+{
+	// Initialize all tiles to Empty
+	for (auto& row : tiles)
+	{
+		row.fill(Empty);
+	}
+	// Set up the tile sprite
+	tile_sprite.setTexture(this->tileset);
+	tile_sprite.setTextureRect(sf::FloatRect({ 0, 0, 16, 16 }));
+}
+
+Level::Level(sf::Texture tileset, std::array<std::array<Tile, 100>, 100> tiles) : tileset(tileset), tiles(tiles)
+{
+	// Set up the tile sprite
+	tile_sprite.setTexture(this->tileset);
+	tile_sprite.setTextureRect(sf::FloatRect({ 0, 0, 16, 16 }));
+}
+
+
+void Level::update(float dt)
+{
+	// TODO: Run all Entity object updates and check for collision
+}
+
+void Level::render(sf::RenderWindow& window)
+{
+	// TODO: Render tiles and Entities
+}
---
+++ src/Environment/Level.h
@@ -0,0 +1,35 @@
+
+#ifndef _NOTTHEPRISONER_LEVEL_HPP_
+#define _NOTTHEPRISONER_LEVEL_HPP_
+
+#include <array>
+#include <SFML/Graphics.hpp>
+
+class Level
+{
+private:
+	enum Tile
+	{
+		Empty,
+		Floor,
+		Wall,
+		Spawn
+	};
+
+	// Rendering/Graphics
+	sf::Texture tileset;
+	sf::Sprite tile_sprite;
+
+	// Level Data
+	std::array<std::array<Tile, 100>, 100> tiles;
+	// TODO: Add player (pointer?) and enemy objects
+
+public:
+	Level(sf::Texture tileset);
+	Level(sf::Texture tileset, std::array<std::array<Tile, 100>, 100> tiles);
+
+	void update(float dt);
+	void render(sf::RenderWindow& window);
+};
+
+#endif // _NOTTHEPRISONER_LEVEL_HPP_
